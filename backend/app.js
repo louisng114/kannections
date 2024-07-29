aba749e82a47219b0ab49bb25e71f5438dc29502
louis <louis@louis-pc.>
2024-07-28 17:13:51 -0700

    change for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Express app for jobly. */
+/** Express app for KANnections. */
 
 const express = require("express");
 const cors = require("cors");
@@ -40,13 +40,16 @@ app.use("/achievements", achievementsRoutes);
 // Proxy endpoint using axios
 app.use('/api/v1/kanji', async (req, res) => {
   try {
-      const apiUrl = new URL('http://ec2-13-60-36-124.eu-north-1.compute.amazonaws.com/api/v1/kanji?size=3000');
-      const response = await axios.get(apiUrl, { responseType: 'stream', params: req.query });
+    const apiUrl = new URL('http://ec2-13-60-36-124.eu-north-1.compute.amazonaws.com/api/v1/kanji?size=3000');
+    const response = await axios.get(apiUrl.href, {
+      responseType: 'json', // Changed from 'stream' to 'json'
+      params: req.query
+    });
 
-      // Pipe the response from the external API to the client
-      response.data.pipe(res);
+    res.status(200).json(response.data); // Send the JSON data directly
   } catch (error) {
-      res.status(500).send('Error proxying request');
+    console.error("Error proxying request:", error.message);
+    res.status(500).json({ error: 'Error proxying request' });
   }
 });
 
